3b3e32cbc9e9eb0b8531a22e53c3d2b59039cd8a
Vitor1Goncalves1 <darknoplayxd@gmail.com>
2024-10-17 21:52:12 -0300

    TDE-AULA-14

---
+++ TDEAula14/IFreavel.cs
@@ -0,0 +1,4 @@
+public interface Ifreavel
+{
+    public void Frear();
+}
---
+++ TDEAula14/Program.cs
@@ -0,0 +1,12 @@
+﻿var carro = new Carro("Volkswagem", "Gol bola 2005");
+carro.Ligar();
+carro.Acelerar();
+carro.Frear();
+carro.Desligar();
+
+Console.WriteLine();
+
+var bicicleta = new Bicicleta("Caloi", "BMX");
+bicicleta.Ligar();
+bicicleta.Acelerar();
+bicicleta.Desligar();
